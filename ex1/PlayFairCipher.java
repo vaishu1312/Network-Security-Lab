a73e2c0207cdd57063245b57a9ffe2394f5a82f0
vaishu1312 <vaishali17181@cse.ssn.edu.in>
2020-09-10 15:19:34 +0530

    added files till 10th sep

---
+++ ex1/PlayFairCipher.java
@@ -0,0 +1,172 @@
+import java.util.Scanner;
+
+public class PlayFairCipher {
+
+    String keyword = new String();
+    String plainText = new String();
+    char key_mat[][] = new char[5][5];
+    String cipherText = new String();
+
+    public void validateKey() {
+        boolean isKeyValid = false;
+        if (keyword.contains("j")) {
+            isKeyValid = true;
+            keyword = keyword.replace('j', 'i');
+        }
+        // remove duplicates
+        String str = new String();
+        for (int i = 0; i < keyword.length(); i++) {
+            char c = keyword.charAt(i);
+            if (str.indexOf(c) < 0)
+                str += c;
+        }
+        if (isKeyValid || !keyword.equals(str)) {
+            keyword = str;
+            System.out.println("Modified key is ------ " + keyword);
+        }
+        // generating matrix entries as a string
+        boolean flag = true;
+        char current;
+        char drop_char = 'j';
+        for (int i = 0; i < 26; i++) {
+            current = (char) (i + 97);
+            if (current == drop_char)
+                continue;
+            for (int j = 0; j < keyword.length(); j++) {
+                if (current == keyword.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+                keyword = keyword + current;
+            flag = true;
+        }
+        // System.out.println("key is " + keyword);
+    }
+
+    public void printKeyMatrix() {
+        System.out.println("\nThe key matrix is\n");
+        int idx = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                key_mat[i][j] = keyword.charAt(idx);
+                System.out.print(key_mat[i][j] + " ");
+                idx++;
+            }
+            System.out.println();
+        }
+    }
+
+    public void modifyPlainText() {
+        if (plainText.contains("j")) {
+            plainText = plainText.replace('j', 'i');
+            System.out.println("After replacing j with i in the plain text ---- " + plainText);
+        }
+        StringBuffer newString = new StringBuffer(plainText);
+
+        for (int i = 0; i < newString.length() - 1; i++) {
+            // check for reptition in a pair
+            if (i % 2 == 0) {
+                if (newString.charAt(i) == newString.charAt(i + 1)) {
+                    if (newString.charAt(i) != 'z')
+                        newString.insert(i + 1, "z");
+                    else
+                        newString.insert(i + 1, "q");
+                }
+
+            }
+        }
+        if (newString.length() % 2 != 0) {
+            if (newString.charAt(newString.length() - 1) != 'z')
+                newString.append("z");
+            else
+                newString.append("q");
+        }
+
+        if (!plainText.equals(newString.toString())) {
+            plainText = newString.toString();
+            System.out.println("Modified plain text is ---- " + plainText);
+        }
+    }
+
+    public int[] getDimensions(char letter) {
+        int[] dimen = new int[2];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (key_mat[i][j] == letter) {
+                    dimen[0] = i;
+                    dimen[1] = j;
+                    break;
+                }
+            }
+        }
+        return dimen;
+    }
+
+    public void encryptMessage() {
+        for (int i = 0; i < plainText.length(); i = i + 2) {
+            int p1[] = new int[2];
+            int p2[] = new int[2];
+            p1 = getDimensions(plainText.charAt(i));
+            p2 = getDimensions(plainText.charAt(i + 1));
+            if (p1[0] == p2[0]) {
+                int c1 = (p1[1] + 1) % 5;
+                int c2 = (p2[1] + 1) % 5;
+                cipherText = cipherText + key_mat[p1[0]][c1] + key_mat[p1[0]][c2];
+            } else if (p1[1] == p2[1]) {
+                int r1 = (p1[0] + 1) % 5;
+                int r2 = (p2[0] + 1) % 5;
+                cipherText = cipherText + key_mat[r1][p1[1]] + key_mat[r2][p1[1]];
+            } else {
+                cipherText = cipherText + key_mat[p1[0]][p2[1]] + key_mat[p2[0]][p1[1]];
+            }
+        }
+    }
+
+    public void decryptMessage() {
+        plainText = "";
+        for (int i = 0; i < cipherText.length(); i = i + 2) {
+            int p1[] = new int[2];
+            int p2[] = new int[2];
+            p1 = getDimensions(cipherText.charAt(i));
+            p2 = getDimensions(cipherText.charAt(i + 1));
+            if (p1[0] == p2[0]) {
+                int c1 = (p1[1] - 1 + 5) % 5;
+                int c2 = (p2[1] - 1 + 5) % 5;
+                plainText = plainText + key_mat[p1[0]][c1] + key_mat[p1[0]][c2];
+            } else if (p1[1] == p2[1]) {
+                int r1 = (p1[0] - 1 + 5) % 5;
+                int r2 = (p2[0] - 1 + 5) % 5;
+                plainText = plainText + key_mat[r1][p1[1]] + key_mat[r2][p1[1]];
+            } else {
+                plainText = plainText + key_mat[p1[0]][p2[1]] + key_mat[p2[0]][p1[1]];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PlayFairCipher pfc = new PlayFairCipher();
+        System.out.println("\nENCRYPTION");
+        System.out.print("\nEnter plain text: ");
+        pfc.plainText = sc.next();
+        pfc.modifyPlainText();
+        System.out.print("\nEnter the key: ");
+        pfc.keyword = sc.next();
+        pfc.validateKey();
+        pfc.printKeyMatrix();
+        pfc.encryptMessage();
+        System.out.println("\nCipher text is: " + pfc.cipherText);
+        System.out.println("\nDECRYPTION");
+        System.out.print("\nEnter cipher text: ");
+        pfc.cipherText = sc.next();
+        System.out.print("\nEnter the key: ");
+        pfc.keyword = sc.next();
+        pfc.validateKey();
+        pfc.printKeyMatrix();
+        pfc.decryptMessage();
+        System.out.println("\nPlain text is: " + pfc.plainText);
+        sc.close();
+    }
+}
---
+++ ex3/RailFence.java
@@ -0,0 +1,141 @@
+import java.util.Scanner;
+
+public class RailFence {
+
+  String plainText = new String();
+  String cipherText = new String();
+  int depth;
+
+  char[][] enc_mat;
+  char[][] dec_mat;
+
+  void encrypt() {
+
+    int len = plainText.length();
+    boolean dir_down = false;
+    int row = 0, col = 0;
+
+    enc_mat = new char[depth][len];
+
+    for (int i = 0; i < depth; i++)
+        for (int j = 0;j < len;j++) 
+            enc_mat[i][j] = '\0';
+
+    for (int i = 0; i < len; i++) {
+
+      enc_mat[row][col++] = plainText.charAt(i);
+      if (row == 0 || row == depth - 1) 
+        dir_down = !dir_down;
+      if (dir_down)
+       row++;
+      else
+       row--;
+
+    }
+
+    System.out.println("\nThe encryption matrix is \n");
+    for (int i = 0; i < depth; i++){
+        for (int j = 0;j < len;j++) {
+            if(enc_mat[i][j] == '\0')
+                System.out.print("  ");
+            else{
+            System.out.print(enc_mat[i][j]+" ");
+            cipherText+=enc_mat[i][j];
+            }
+        }
+        System.out.print("\n");
+    }
+
+    System.out.println("\nThe cipher text is "+cipherText);
+} 
+
+void decrypt() 
+{ 
+    int len = cipherText.length();
+
+    boolean dir_down = false;
+    int row = 0, col = 0;
+
+    dec_mat = new char[depth][len];        
+
+    for (int i=0; i < len; i++) 
+    { 
+        dec_mat[row][col++] = '*'; 
+
+        if (row == 0 || row == depth - 1) 
+                  dir_down = !dir_down;
+        if (dir_down)
+            row++;
+        else
+            row--;
+    } 
+  
+    int index = 0; 
+    System.out.println("\nThe decryption matrix is \n");
+
+    for (int i=0; i<depth; i++) {
+        for (int j=0; j<len; j++) {
+            if (dec_mat[i][j] == '*' && index<len) {
+                dec_mat[i][j] = cipherText.charAt(index++); 
+                System.out.print(dec_mat[i][j]+" ");
+            }
+            else{
+                System.out.print("  ");
+            }
+        }
+        System.out.print("\n");
+    }
+    
+    row = 0;
+    col = 0; 
+    plainText="";
+
+    for (int i=0; i< len; i++) 
+    { 
+        if (dec_mat[row][col] != '*') 
+           plainText+=dec_mat[row][col++];
+        
+        if (row == 0 || row == depth - 1) 
+           dir_down = !dir_down;
+        if (dir_down)
+          row++;
+        else
+          row--;
+    } 
+
+    System.out.println("\nThe plain text is "+plainText);
+    
+} 
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    RailFence rf = new RailFence();
+    System.out.println("\nRAIL FENCE CIPHER");
+    System.out.println("\nENCRYPTION");
+    System.out.println("**********");
+    System.out.print("\nEnter plain plainText: ");
+    rf.plainText = sc.next();
+    System.out.print("\nEnter the depth(key): ");
+    rf.depth = sc.nextInt();
+    while (rf.depth<=0 || rf.depth>=rf.plainText.length()) {
+        System.out.println("\nInvalid key");
+        System.out.print("\nEnter the depth(key): ");
+        rf.depth = sc.nextInt();
+    }
+    rf.encrypt();
+
+    System.out.println("\nDECRYPTION");
+    System.out.println("**********");
+    System.out.print("\nEnter cipher plainText: ");
+    rf.cipherText = sc.next();
+    System.out.print("\nEnter the depth(key): ");
+    rf.depth = sc.nextInt();
+    while (rf.depth<=0 || rf.depth>=rf.plainText.length()) {
+        System.out.println("\nInvalid key");
+        System.out.print("\nEnter the depth(key): ");
+        rf.depth = sc.nextInt();
+    }
+    rf.decrypt();
+    sc.close();
+  }
+}
